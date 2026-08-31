051fb6353e25751e232a748ee851ff1ab6d2cee6
Anshita <anshitaaaseri@gmail.com>
2026-08-31 19:42:28 +0530

    Delete auth-service directory

--- auth-service/src/main/java/com/freelance/auth/repository/CertificationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.freelance.auth.repository;
-
-import com.freelance.auth.entity.Certification;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface CertificationRepository extends JpaRepository<Certification, Long> {
-    List<Certification> findByUserId(Long userId);
-}
